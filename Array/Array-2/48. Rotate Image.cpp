872db5c1911533234dbbe763fd34e27d91a0e50f
Astha Sahani <142304973+Astha86@users.noreply.github.com>
2024-08-25 21:28:07 +0530

    Add files via upload

---
+++ Array/Array-2/48. Rotate Image.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int r = matrix.size();
+        int c = matrix[0].size();
+        
+        //  Step-1: Transpose the matrix
+        for(int i=0; i<r; i++){
+            for(int j=0; j<i; j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+
+        //  Step-2: Reverse the matrix
+        for(int i=0; i<r; i++){
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
+    }
+};
