98ac9f8e6c34f8fa9875c8735cf875ab60d8e2de
Astha Sahani <142304973+Astha86@users.noreply.github.com>
2024-09-06 00:07:57 +0530

    Added time and space complexity

--- Array/Array-4/3. Longest Substring Without Repeating Characters.cpp
+++ Array/Array-4/3. Longest Substring Without Repeating Characters.cpp
@@ -2,16 +2,21 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         vector <int> mpp(200, -1);
-        int left = 0, right = 0;
+        int l = 0;
+        int r = 0;
         int n = s.size();
-        int len = 0;
-        while(right < n) {
-            if (mpp[s[right]] != -1)
-            left = max(mpp[s[right]] + 1, left);
-            mpp[s[right]] = right;
-            len = max(len, right - left + 1);
-            right++;
+        int ans = 0;
+        while(r < n) {
+            if (mpp[s[r]] != -1)
+            l = max(mpp[s[r]] + 1, l);
+            mpp[s[r]] = r;
+            int len = r - l + 1;
+            ans = max(ans, len);
+            r++;
         }
-        return len;
+        return ans;
     }
 };
+
+// T.C = O(n)
+// S.C = O(n)
